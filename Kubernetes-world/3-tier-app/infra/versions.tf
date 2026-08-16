af5e78fcbf6da0037d9d92f009754880437b5327
Devops with Akhilesh <livingdevops@gmail.com>
2026-08-16 15:24:35 +0530

    asd

--- Kubernetes-world/3-tier-app/infra/versions.tf
+++ Kubernetes-world/3-tier-app/infra/versions.tf
@@ -9,6 +9,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0"
+    }
   }
 }
 
